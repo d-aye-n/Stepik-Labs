5431f5dbe81bfcfcca405245dffd19c8c055fdc8
Daniil <90516318+d-aye-n@users.noreply.github.com>
2024-02-12 00:09:11 +0300

    Lab3 OOP К проверке

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+# Не смог исправить проблему с кодировкой при которой нормально не отображается кириллица
+# Не понимаю почему при вызове методов __str__ и __repr__ возникает бесконечный цикл
 class Book:
     """ Базовый класс книги. """
 
@@ -13,7 +14,7 @@ class Book:
         return self._author
 
     def __str__(self):
-        return f"Êíèãà {self.name}. Àâòîð {self.author}"
+        return f"Книга {self.name}. Автор {self.author}"
 
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
@@ -27,10 +28,10 @@ class PaperBook(Book):
 
     def pages(self, value):
         if not isinstance(value, int) or value <= 0:
-            raise ValueError("Êîëè÷åñòâî ñòðàíèö äîëæíî áûòü ïîëîæèòåëüíûì öåëûì ÷èñëîì.")
+            raise ValueError("Количество страниц должно быть положительным целым числом.")
         self.pages = value
 
-    # Ìåòîäû íóæíî ïåðåãðóçèòü òàê êàê â áàçîâîì êëàññå îñòóòñòâóåò àòðèáóò pages
+    # Методы нужно перегрузить так как в базовом классе остутствует атрибут pages
 
 
 class AudioBook(Book):
@@ -40,12 +41,13 @@ class AudioBook(Book):
 
     def duration(self, value):
         if not isinstance(value, (int, float)) or value <= 0:
-            raise ValueError("Äëèòåëüíîñòü äîëæíà áûòü ïîëîæèòåëüíûì ÷èñëîì.")
+            raise ValueError("Длительность должна быть положительным числом.")
         self.duration = value
 
-    # Ìåòîäû íóæíî ïåðåãðóçèòü òàê êàê â áàçîâîì êëàññå îñòóòñòâóåò àòðèáóò duration
+    # Методы нужно перегрузить так как в базовом классе остутствует атрибут duration
     def __str__(self):
-        return f"Êíèãà {self.name}. Àâòîð {self.author}. Äëèòåëüíîñòü: {self.duration} ÷àñîâ"
+        return f"Книга {self.name}. Автор {self.author}. Длительность: {self.duration} часов"
 
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+
